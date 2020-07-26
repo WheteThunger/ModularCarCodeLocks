6038c3d021178a2abff7a07311bf7c5eafbb8079
WhiteThunder <whetethunger@gmail.com>
2020-07-25 22:19:53 -0700

    Initial commit

---
+++ CarCodeLocks.cs
@@ -0,0 +1,472 @@
+﻿using System;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Oxide.Core;
+using Oxide.Core.Libraries.Covalence;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Modular Car Code Locks", "WhiteThunder", "1.0.0")]
+    [Description("Allows players to deploy code locks to Modular Cars.")]
+    internal class CarCodeLocks : CovalencePlugin
+    {
+        #region Fields
+
+        private PluginConfig pluginConfig;
+
+        private const string PermissionUse = "carcodelocks.use";
+        private const string PermissionFreeLock = "carcodelocks.free";
+
+        private const string CodeLockPrefab = "assets/prefabs/locks/keypad/lock.code.prefab";
+        private const string CodeLockDeployedEffectPrefab = "assets/prefabs/locks/keypad/effects/lock-code-deploy.prefab";
+
+        private const int CodeLockItemId = 1159991980;
+
+        private readonly Vector3 CodeLockPosition = new Vector3(-0.9f, 0.35f, -0.5f);
+
+        private CooldownManager Cooldowns;
+
+        #endregion
+
+        #region Hooks
+
+        private void Init()
+        {
+            pluginConfig = Config.ReadObject<PluginConfig>();
+
+            permission.RegisterPermission(PermissionUse, this);
+            permission.RegisterPermission(PermissionFreeLock, this);
+
+            Cooldowns = new CooldownManager(pluginConfig.CooldownSeconds);
+        }
+
+        private void OnEntityKill(VehicleModuleSeating seatingModule)
+        {
+            var car = seatingModule.Vehicle as ModularCar;
+            if (car == null) return;
+
+            var codeLock = seatingModule.GetComponentInChildren<CodeLock>();
+            if (codeLock == null) return;
+
+            // Try to move the lock to another cockpit module
+            codeLock.SetParent(null);
+            NextTick(() =>
+            {
+                var driverModule = car != null ? FindFirstDriverModule(car) : null;
+                if (driverModule != null)
+                    codeLock.SetParent(driverModule);
+                else
+                    codeLock.Kill();
+            });
+        }
+
+        object CanMountEntity(BasePlayer player, BaseVehicleMountPoint entity)
+        {
+            var car = entity?.GetVehicleParent() as ModularCar;
+            return CanPlayerInteractWithCar(player, car);
+        }
+
+        object CanLootEntity(BasePlayer player, StorageContainer container)
+        {
+            var parent = container.GetParentEntity();
+            var car = parent as ModularCar ?? (parent as VehicleModuleStorage)?.Vehicle as ModularCar;
+            return CanPlayerInteractWithCar(player, car);
+        }
+
+        object CanLootEntity(BasePlayer player, LiquidContainer container)
+        {
+            var car = (container.GetParentEntity() as VehicleModuleStorage)?.Vehicle as ModularCar;
+            return CanPlayerInteractWithCar(player, car);
+        }
+
+        object CanLootEntity(BasePlayer player, ModularCarGarage carLift)
+        {
+            if (pluginConfig.AllowEditingWhileLockedOut || !carLift.PlatformIsOccupied) return null;
+            return CanPlayerInteractWithCar(player, carLift.carOccupant);
+        }
+
+        private object CanPlayerInteractWithCar(BasePlayer player, ModularCar car)
+        {
+            if (car == null) return null;
+
+            var codeLock = GetCarCodeLock(car);
+            if (codeLock == null || IsPlayerAuthorizedToCodeLock(player, codeLock)) return null;
+
+            Effect.server.Run(codeLock.effectDenied.resourcePath, codeLock, 0, Vector3.zero, Vector3.forward);
+            player.ChatMessage(GetMessage(player.IPlayer, "Error.CarLocked"));
+
+            return false;
+        }
+
+        #endregion
+
+        #region API
+
+        private CodeLock API_DeployCodeLock(ModularCar car, BasePlayer player)
+        {
+            if (car == null || car.IsDead() || DeployWasBlocked(car, player) || GetCarCodeLock(car) != null) return null;
+
+            if (player != null)
+                return DeployCodeLockForPlayer(car, player, isFree: true);
+
+            var driverModule = FindFirstDriverModule(car);
+            if (driverModule == null) return null;
+
+            return DeployCodeLock(car, driverModule);
+        }
+
+        #endregion
+
+        #region Commands
+
+        [Command("carcodelock", "ccl")]
+        private void CarCodeLockCommand(IPlayer player, string cmd, string[] args)
+        {
+            if (player.IsServer) return;
+
+            ModularCar car;
+            if (!VerifyPermissionAny(player, PermissionUse) ||
+                !VerifyNotBuildingBlocked(player) ||
+                !VerifyCarFound(player, out car) ||
+                !VerifyCarIsNotDead(player, car) ||
+                !VerifyNoOwnershipRestriction(player, car) ||
+                !VerifyCarHasNoLock(player, car) ||
+                !VerifyCarCanHaveALock(player, car) ||
+                !VerifyPlayerHasLockOrResources(player) ||
+                !VerifyOffCooldown(player))
+                return;
+
+            var basePlayer = player.Object as BasePlayer;
+            if (DeployWasBlocked(car, basePlayer)) return;
+
+            var codeLock = DeployCodeLockForPlayer(car, basePlayer, isFree: player.HasPermission(PermissionFreeLock));
+            if (codeLock == null) return;
+
+            Cooldowns.UpdateLastUsedForPlayer(player.Id);
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private bool DeployWasBlocked(ModularCar car, BasePlayer player)
+        {
+            object hookResult = Interface.CallHook("CanDeployCarCodeLock", car, player);
+            return (hookResult is bool && (bool)hookResult == false);
+        }
+
+        private bool VerifyPermissionAny(IPlayer player, params string[] permissionNames)
+        {
+            foreach (var perm in permissionNames)
+            {
+                if (!permission.UserHasPermission(player.Id, perm))
+                {
+                    ReplyToPlayer(player, "Error.NoPermission");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool VerifyNotBuildingBlocked(IPlayer player)
+        {
+            if (!(player.Object as BasePlayer).IsBuildingBlocked()) return true;
+            ReplyToPlayer(player, "Error.BuildingBlocked");
+            return false;
+        }
+
+        private bool VerifyCarFound(IPlayer player, out ModularCar car)
+        {
+            var basePlayer = player.Object as BasePlayer;
+            var entity = GetLookEntity(basePlayer);
+
+            if (pluginConfig.AllowDeployOffLift)
+            {
+                car = entity as ModularCar;
+                if (car != null) return true;
+
+                // Check for a lift as well since sometimes it blocks the ray
+                car = (entity as ModularCarGarage)?.carOccupant;
+                if (car != null) return true;
+
+                ReplyToPlayer(player, "Error.NoCarFound");
+                return false;
+            }
+
+            car = (entity as ModularCarGarage)?.carOccupant;
+            if (car != null) return true;
+
+            var carEnt = entity as ModularCar;
+            if (carEnt == null)
+            {
+                ReplyToPlayer(player, "Error.NoCarFound");
+                return false;
+            }
+
+            if (!IsCarOnLift(carEnt))
+            {
+                ReplyToPlayer(player, "Error.NotOnLift");
+                return false;
+            }
+
+            car = carEnt;
+            return true;
+        }
+
+        private bool VerifyCarIsNotDead(IPlayer player, ModularCar car)
+        {
+            if (!car.IsDead()) return true;
+            ReplyToPlayer(player, "Error.CarDead");
+            return false;
+        }
+
+        private bool VerifyNoOwnershipRestriction(IPlayer player, ModularCar car)
+        {
+            if (pluginConfig.AllowLockingCarsOwnedByOthers || car.OwnerID == 0 || car.OwnerID.ToString() == player.Id) return true;
+            ReplyToPlayer(player, "Error.OwnedByOther");
+            return false;
+        }
+
+        private bool VerifyCarHasNoLock(IPlayer player, ModularCar car)
+        {
+            if (GetCarCodeLock(car) == null) return true;
+            ReplyToPlayer(player, "Error.HasLock");
+            return false;
+        }
+
+        private bool VerifyCarCanHaveALock(IPlayer player, ModularCar car)
+        {
+            if (car.carLock.CanHaveALock()) return true;
+            ReplyToPlayer(player, "Error.NoCockpit");
+            return false;
+        }
+
+        private bool VerifyPlayerHasLockOrResources(IPlayer player)
+        {
+            if (player.HasPermission(PermissionFreeLock)) return true;
+
+            var playerInventory = (player.Object as BasePlayer).inventory;
+            if (playerInventory.FindItemID(CodeLockItemId) != null) return true;
+
+            var itemCost = pluginConfig.CodeLockCost;
+            var itemDefinition = itemCost.GetItemDefinition();
+            if (playerInventory.GetAmount(itemDefinition.itemid) >= itemCost.Amount) return true;
+
+            ReplyToPlayer(player, "Error.InsufficientResources", itemCost.Amount, itemDefinition.displayName.translated);
+            return false;
+        }
+
+        private bool VerifyOffCooldown(IPlayer player)
+        {
+            var secondsRemaining = Cooldowns.GetSecondsRemaining(player.Id);
+            if (secondsRemaining <= 0) return true;
+            ReplyToPlayer(player, "Error.Cooldown", Math.Ceiling(secondsRemaining));
+            return false;
+        }
+
+        private BaseEntity GetLookEntity(BasePlayer player)
+        {
+            RaycastHit hit;
+            if (!Physics.Raycast(player.eyes.HeadRay(), out hit, 3)) return null;
+            return hit.GetEntity();
+        }
+
+        private bool IsCarOnLift(ModularCar car)
+        {
+            RaycastHit hitInfo;
+            // This isn't perfect as it can hit other deployables such as rugs
+            if (!Physics.SphereCast(car.transform.position + Vector3.up, 1f, Vector3.down, out hitInfo, 1f)) return false;
+
+            var lift = RaycastHitEx.GetEntity(hitInfo) as ModularCarGarage;
+            return lift != null && lift.carOccupant == car;
+        }
+
+        private CodeLock DeployCodeLockForPlayer(ModularCar car, BasePlayer player, bool isFree = true)
+        {
+            var driverModule = FindFirstDriverModule(car);
+            if (driverModule == null) return null;
+
+            var codeLockItem = player.inventory.FindItemID(CodeLockItemId);
+            if (codeLockItem == null && !isFree)
+            {
+                var itemCost = pluginConfig.CodeLockCost;
+                if (itemCost.Amount > 0)
+                    player.inventory.Take(null, itemCost.GetItemID(), itemCost.Amount);
+            }
+
+            var codeLock = DeployCodeLock(car, driverModule, player.userID);
+            if (codeLock == null) return null;
+
+            // Allow other plugins to detect the lock being deployed (e.g., auto lock)
+            if (codeLockItem != null)
+            {
+                Interface.CallHook("OnItemDeployed", codeLockItem.GetHeldEntity(), car);
+                if (!isFree)
+                    player.inventory.Take(null, CodeLockItemId, 1);
+            }
+            else
+            {
+                // Temporarily increase the player inventory capacity to ensure there is enough space
+                player.inventory.containerMain.capacity++;
+                var temporaryLockItem = ItemManager.CreateByItemID(CodeLockItemId);
+                if (player.inventory.GiveItem(temporaryLockItem))
+                {
+                    Interface.CallHook("OnItemDeployed", temporaryLockItem.GetHeldEntity(), car);
+                    temporaryLockItem.RemoveFromContainer();
+                }
+                temporaryLockItem.Remove();
+                player.inventory.containerMain.capacity--;
+            }
+
+            return codeLock;
+        }
+
+        private CodeLock DeployCodeLock(ModularCar car, VehicleModuleSeating driverModule, ulong ownerID = 0)
+        {
+            var codeLock = GameManager.server.CreateEntity(CodeLockPrefab, CodeLockPosition, Quaternion.identity) as CodeLock;
+            if (codeLock == null) return null;
+
+            if (ownerID != 0)
+                codeLock.OwnerID = ownerID;
+
+            codeLock.SetParent(driverModule);
+            codeLock.Spawn();
+            car.SetSlot(BaseEntity.Slot.Lock, codeLock);
+
+            Effect.server.Run(CodeLockDeployedEffectPrefab, codeLock.transform.position);
+
+            return codeLock;
+        }
+
+        private CodeLock GetCarCodeLock(ModularCar car) =>
+            car.GetSlot(BaseEntity.Slot.Lock) as CodeLock;
+
+        private bool IsPlayerAuthorizedToCodeLock(BasePlayer player, CodeLock codeLock) =>
+            !codeLock.IsLocked() ||
+            codeLock.whitelistPlayers.Contains(player.userID) ||
+            codeLock.guestPlayers.Contains(player.userID);
+
+        private VehicleModuleSeating FindFirstDriverModule(ModularCar car)
+        {
+            for (int socketIndex = 0; socketIndex < car.TotalSockets; socketIndex++)
+            {
+                BaseVehicleModule module;
+                if (car.TryGetModuleAt(socketIndex, out module))
+                {
+                    var seatingModule = module as VehicleModuleSeating;
+                    if (seatingModule != null && seatingModule.HasADriverSeat())
+                        return seatingModule;
+                }
+            }
+            return null;
+        }
+
+        internal class CooldownManager
+        {
+            private readonly Dictionary<string, float> CooldownMap = new Dictionary<string, float>();
+            private readonly float CooldownDuration;
+
+            public CooldownManager(float duration)
+            {
+                CooldownDuration = duration;
+            }
+
+            public void UpdateLastUsedForPlayer(string userID)
+            {
+                if (CooldownMap.ContainsKey(userID))
+                    CooldownMap[userID] = Time.realtimeSinceStartup;
+                else
+                    CooldownMap.Add(userID, Time.realtimeSinceStartup);
+            }
+
+            public float GetSecondsRemaining(string userID)
+            {
+                if (!CooldownMap.ContainsKey(userID)) return 0;
+                return CooldownMap[userID] + CooldownDuration - Time.realtimeSinceStartup;
+            }
+        }
+
+        #endregion
+
+        #region Configuration
+
+        internal class PluginConfig
+        {
+            [JsonProperty("AllowDeployOffLift")]
+            public bool AllowDeployOffLift = false;
+
+            [JsonProperty("AllowEditingWhileLockedOut")]
+            public bool AllowEditingWhileLockedOut = true;
+
+            [JsonProperty("AllowLockingCarsOwnedByOthers")]
+            public bool AllowLockingCarsOwnedByOthers = false;
+
+            [JsonProperty("CooldownSeconds")]
+            public float CooldownSeconds = 10.0f;
+
+            [JsonProperty("CodeLockCost")]
+            public ItemCost CodeLockCost = new ItemCost
+            {
+                ItemShortName = "metal.fragments",
+                Amount = 100,
+            };
+        }
+
+        internal class ItemCost
+        {
+            [JsonProperty("ItemShortName")]
+            public string ItemShortName;
+
+            [JsonProperty("Amount")]
+            public int Amount;
+
+            public ItemDefinition GetItemDefinition() =>
+                ItemManager.FindItemDefinition(ItemShortName);
+
+            public int GetItemID() =>
+                GetItemDefinition().itemid;
+        }
+
+        private PluginConfig GetDefaultConfig() =>
+            new PluginConfig();
+
+        protected override void LoadDefaultConfig() =>
+            Config.WriteObject(GetDefaultConfig(), true);
+
+        #endregion
+
+        #region Localization
+
+        private void ReplyToPlayer(IPlayer player, string messageName, params object[] args) =>
+            player.Reply(string.Format(GetMessage(player, messageName), args));
+
+        private string GetMessage(IPlayer player, string messageName, params object[] args)
+        {
+            var message = lang.GetMessage(messageName, this, player.Id);
+            return args.Length > 0 ? string.Format(message, args) : message;
+        }
+
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                ["Error.NoPermission"] = "You don't have permission to use this command.",
+                ["Error.BuildingBlocked"] = "Error: Cannot do that while building blocked.",
+                ["Error.NoCarFound"] = "Error: No car found.",
+                ["Error.CarDead"] = "Error: That car is dead.",
+                ["Error.OwnedByOther"] = "Error: That car is owned by someone else.",
+                ["Error.NotOnLift"] = "Error: That car must be on a lift to receive a lock.",
+                ["Error.HasLock"] = "Error: That car already has a lock.",
+                ["Error.NoCockpit"] = "Error: That car needs a driver seat to receive a lock.",
+                ["Error.InsufficientResources"] = "Error: You need <color=red>{0} {1}</color> to craft a lock.",
+                ["Error.Cooldown"] = "Please wait <color=red>{0}s</color> and try again.",
+                ["Error.CarLocked"] = "That vehicle is locked.",
+            }, this, "en");
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,95 @@
+**Modular Car Code Locks** allows players to deploy code locks to Modular Cars.
+
+- Similar to key locks, players that do not have authorization to the code lock cannot access any of the car's features.
+- By default, you can only deploy a code lock to a car that is on a lift. This is configurable.
+- You cannot deploy a code lock to a car while building blocked.
+- If you have a code lock in your inventory, it will be used, or else one will be crafted if you have the resources. Locks can be free with additional permissions.
+- When the module that the lock is deployed to is removed at a lift, the lock will automatically be moved to another driver seat module. If all driver seat modules are removed, the lock is removed as well, which is consistent with key lock behavior.
+- If you have both a key lock and code lock on a car, you will need both the key and the code to access the car. This is not recommended because it defeats the purpose of having the code lock.
+
+## Commands
+
+- `carcodelock` (or `ccl`) -- Deploy a code lock to the car you are aiming at. You must be within several meters of the car. You can also aim at a lift to target the car currently on it.
+
+## Permissions
+
+- `carcodelocks.use` -- Allows the player to deploy code locks to cars.
+- `carcodelocks.free` -- Allows the player to deploy code locks to cars without consuming locks or resources from their inventory.
+
+## Configuration
+```json
+{
+  "AllowDeployOffLift": false,
+  "AllowEditingWhileLockedOut": true,
+  "AllowLockingCarsOwnedByOthers": false,
+  "CodeLockCost": {
+    "Amount": 100,
+    "ItemShortName": "metal.fragments"
+  },
+  "CooldownSeconds": 10.0
+}
+```
+
+- `AllowDeployOffLift` (`true` or `false`) -- Whether to allow players to deploy code locks to cars that are not currently on a lift. This is `false` by default to be consistent with how key locks work.
+- `AllowEditingWhileLockedOut` (`true` or `false`) -- Whether to allow players to edit cars at lifts when they are not authorized to the car's code lock. This is `true` by default to be consistent with how key locks work.
+- `AllowLockingCarsOwnedByOthers` (`true` or `false`) -- Whether to allow players to deploy code locks to cars owned by other players (e.g., cars spawned by mods). Regardless of this setting, players can always deploy code locks to their own cars or randomly spawned cars.
+- `CodeLockCost` -- The amount to charge a player when crafting a code lock automatically.
+- `CooldownSeconds` -- Cooldown to prevent players from using this plugin to make locks faster than the game naturally allows with crafting. Configure this based on the crafting speed of locks on your server.
+
+## Localization
+
+```json
+{
+  "Error.NoPermission": "You don't have permission to use this command.",
+  "Error.BuildingBlocked": "Error: Cannot do that while building blocked.",
+  "Error.NoCarFound": "Error: No car found.",
+  "Error.CarDead": "Error: That car is dead.",
+  "Error.OwnedByOther": "Error: That car is owned by someone else.",
+  "Error.NotOnLift": "Error: That car must be on a lift to receive a lock.",
+  "Error.HasLock": "Error: That car already has a lock.",
+  "Error.NoCockpit": "Error: That car needs a driver seat to receive a lock.",
+  "Error.InsufficientResources": "Error: You need <color=red>{0} {1}</color> to craft a lock.",
+  "Error.Cooldown": "Please wait <color=red>{0}s</color> and try again.",
+  "Error.CarLocked": "That vehicle is locked."
+}
+```
+
+## Developer API
+
+#### API_DeployCodeLock
+
+Plugins can call this API to deploy a code lock to a modular car. The `BasePlayer` parameter is optional, but providing it is recommended as it allows for potential compatibility with auto-lock plugins. Deploying a lock this way will not consume any of the player's items.
+
+```csharp
+CodeLock API_DeployCodeLock(ModularCar car, BasePlayer player)
+```
+
+The return value will be the newly deployed lock, or `null` if a lock was not deployed for any of the following reasons.
+- The car was destroyed or is "dead"
+- The car has no driver seats
+- The car already has a code lock
+- Another plugin blocked it with a hook
+
+## Hooks
+
+#### CanDeployCarCodeLock
+
+- Called when a player or a plugin tries to deploy a code lock to a modular car.
+- Returning `false` will prevent the code lock from being deployed. If attempted by a player, none of their items will be consumed.
+- Returning `null` results in the default behavior.
+
+```csharp
+bool CanDeployCarCodeLock(ModularCar car, BasePlayer player)
+```
+
+Note: The `BasePlayer` parameter may be `null` if another plugin initiated the code lock deployment without providing a player.
+
+#### OnItemDeployed
+
+This is an Oxide hook that is normally called when deploying a code lock or other deployable. It's also explicitly called by this plugin to allow for compatibility with other plugins.
+
+```csharp
+void OnItemDeployed(Deployer deployer, BaseEntity entity)
+```
+
+Note: The `BaseEntity` parameter will be the `ModularCar` instance.
